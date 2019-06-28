5d0f412aae84d39ee3868a090cf9ec35cc682ab2
angvidal <angvidal@publicisgroupe.net>
2019-06-28 19:31:13 -0400

    just removed unecesary stuff and did mobile styles

--- js/carousel.js
+++ js/carousel.js
@@ -1,58 +1,58 @@
-var images = [
-"photos/cheese-one.jpg",
-"photos/front.jpg",
-"photos/got-cheese.jpg",
-"photos/grocery-five.jpg",
-"photos/grocery-four.jpg",
-"photos/grocery-one.jpg",
-"photos/grocery-six.jpg",
-"photos/grocery-three.jpg",
-"photos/grocery-two.jpg",
-"photos/history-one.jpg",
-"photos/history-two.jpg",
-"photos/italian-ice.jpg",
-"photos/jessie-one.jpg",
-"photos/jessie-two.jpg",
-"photos/menu-two.jpeg",
-"photos/menu.jpeg",
-"photos/opening-one.jpg",
-"photos/opening-two.jpg",
-"photos/pasta-one.jpg",
-"photos/opening-two.jpg",
-"photos/pasta-two.jpg",
-"photos/store-front.jpeg",
-"photos/sub-one.jpg",
-"photos/sub-two.jpg",
-"photos/sub-three.jpg",
-"photos/sub-four.jpg",
-"photos/sub-five.jpg",
-"photos/sub-six.jpg",
-"photos/sub-seven.jpg",
-"photos/sub-eight.jpg",
-"photos/sub-nine.jpg",
-"photos/sub-ten.jpg",
-"photos/tea.jpg",
-"photos/whole-store.jpeg"
-];
-
-var img = document.getElementById("slider-img");
-img.setAttribute("src", images[0])
-
-var total = images.length;
-var imagecounter = 0;
-
-function slider () {
-
-  if (imagecounter <= total) {
-    imagecounter++;
-  }
-
-  if (imagecounter == total) {
-    imagecounter = 0;
-  }
-
-document.getElementById("slider-img");
-img.setAttribute("src", images[imagecounter]);
-}
-
-setInterval(slider, 3000)
+// var images = [
+// "photos/cheese-one.jpg",
+// "photos/front.jpg",
+// "photos/got-cheese.jpg",
+// "photos/grocery-five.jpg",
+// "photos/grocery-four.jpg",
+// "photos/grocery-one.jpg",
+// "photos/grocery-six.jpg",
+// "photos/grocery-three.jpg",
+// "photos/grocery-two.jpg",
+// "photos/history-one.jpg",
+// "photos/history-two.jpg",
+// "photos/italian-ice.jpg",
+// "photos/jessie-one.jpg",
+// "photos/jessie-two.jpg",
+// "photos/menu-two.jpeg",
+// "photos/menu.jpeg",
+// "photos/opening-one.jpg",
+// "photos/opening-two.jpg",
+// "photos/pasta-one.jpg",
+// "photos/opening-two.jpg",
+// "photos/pasta-two.jpg",
+// "photos/store-front.jpeg",
+// "photos/sub-one.jpg",
+// "photos/sub-two.jpg",
+// "photos/sub-three.jpg",
+// "photos/sub-four.jpg",
+// "photos/sub-five.jpg",
+// "photos/sub-six.jpg",
+// "photos/sub-seven.jpg",
+// "photos/sub-eight.jpg",
+// "photos/sub-nine.jpg",
+// "photos/sub-ten.jpg",
+// "photos/tea.jpg",
+// "photos/whole-store.jpeg"
+// ];
+//
+// var img = document.getElementById("slider-img");
+// img.setAttribute("src", images[0])
+//
+// var total = images.length;
+// var imagecounter = 0;
+//
+// function slider () {
+//
+//   if (imagecounter <= total) {
+//     imagecounter++;
+//   }
+//
+//   if (imagecounter == total) {
+//     imagecounter = 0;
+//   }
+//
+// document.getElementById("slider-img");
+// img.setAttribute("src", images[imagecounter]);
+// }
+//
+// setInterval(slider, 3000)
