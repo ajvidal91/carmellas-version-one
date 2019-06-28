cb6685417ef43dd2d783e6e60fab40a3bc55b73c
angvidal <angvidal@publicisgroupe.net>
2019-06-28 15:37:05 -0400

    added about section

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,39 @@
-var images = ["photos/front.jpg","photos/cheese-one.jpg", "photos/grocery-five.jpg",
-"photos/grocery-six.jpg", "photos/got-cheese.jpg","photos/grocery-one.jpg","photos/grocery-five.jpg",
-"photos/history-one.jpg","photos/jessie-one.jpg","photos/grocery-one.jpg","photos/sub-one.jpg",
-"photos/sub-two.jpg","photos/sub-three.jpg","photos/sub-four.jpg","photos/sub-five.jpg"];
+var images = [
+"photos/cheese-one.jpg",
+"photos/front.jpg",
+"photos/got-cheese.jpg",
+"photos/grocery-five.jpg",
+"photos/grocery-four.jpg",
+"photos/grocery-one.jpg",
+"photos/grocery-six.jpg",
+"photos/grocery-three.jpg",
+"photos/grocery-two.jpg",
+"photos/history-one.jpg",
+"photos/history-two.jpg",
+"photos/italian-ice.jpg",
+"photos/jessie-one.jpg",
+"photos/jessie-two.jpg",
+"photos/menu-two.jpeg",
+"photos/menu.jpeg",
+"photos/opening-one.jpg",
+"photos/opening-two.jpg",
+"photos/pasta-one.jpg",
+"photos/opening-two.jpg",
+"photos/pasta-two.jpg",
+"photos/store-front.jpeg",
+"photos/sub-one.jpg",
+"photos/sub-two.jpg",
+"photos/sub-three.jpg",
+"photos/sub-four.jpg",
+"photos/sub-five.jpg",
+"photos/sub-six.jpg",
+"photos/sub-seven.jpg",
+"photos/sub-eight.jpg",
+"photos/sub-nine.jpg",
+"photos/sub-ten.jpg",
+"photos/tea.jpg",
+"photos/whole-store.jpeg"
+];
 
 var img = document.getElementById("slider-img");
 img.setAttribute("src", images[0])
